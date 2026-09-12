059b454f9e2b379632a5c5253209bbb77113532d
thugan <thuganenko@gmail.com>
2026-09-12 12:20:41 +0300

    starter code

--- Project1/first.cpp
+++ Project1/first.cpp
@@ -1,8 +1,23 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
 
 void main()
 {
-	cout << "Hello world" << endl;
+	cout << "I\n";
+	cout << "\tlove\n";
+	cout << "\t\tyou\n";
+	cout << "\t\t\tC++\n";
+	cout << "\n";
+	cout << "\t\t...:Resume:...\n";
+	cout << "\tName: \t\t\tAhnesa\n";
+	cout << "\tSurname: \t\tAleksiichuk\n";
+	cout << "\tLast name: \t\tRomanivna\n";
+	cout << "\n";
+	cout << "\tDate of birth: \t\t15.12.2009\n";
+	cout << "\tCity: \t\t\tRivne\n"; 
+	cout << "\tAge: \t\t\t16\n";
+	cout << "\n";
+	cout << "\tHobby:  \t\t\tC++\n";
+	cout << ".....................................................";
 }
 
